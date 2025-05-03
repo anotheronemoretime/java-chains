8a1c152a88b3d677ec1fa9721fdf24f3e3c91dc4
Your Name <you@example.com>
2025-05-03 17:38:20 +0200

    update

--- run-java-chains.sh
+++ run-java-chains.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ "$1" == "start" ]; then
-    docker-compose up -d
+    docker compose up -d
     echo "[+] Login page http://$(hostname -I | awk '{print $1}'):8011"
 elif [ "$1" == "stop" ]; then
-    docker-compose down
+    docker compose down
 elif [ "$1" == "get-password" ]; then
     docker logs $(docker ps | grep javachains/javachains | awk '{print $1}') | grep -E 'password'
 else
